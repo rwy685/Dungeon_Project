758609301dd57915b07bccc7a7f5e1931d21ac3a
rwy685 <rwy685@gmail.com>
2025-11-10 17:28:32 +0900

    feat.점프대 신에 추가

---
+++ DungeonExit/Assets/Scripts/FieldObject/JumpFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpFloor : MonoBehaviour
+{
+    public float jumpPower = 6f;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody playerRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (playerRb != null)
+            {
+                playerRb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+        }
+    }
+
+}
