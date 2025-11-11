64843ecece791349466063199d7ffad0882f6554
rwy685 <rwy685@gmail.com>
2025-11-11 10:46:07 +0900

    feat.SawBlade Compound콜라이더 형식으로 변경 및 넉백구현

--- DungeonExit/Assets/Scripts/FieldObject/DamageZone.cs
+++ DungeonExit/Assets/Scripts/FieldObject/DamageZone.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
-    [SerializeField] private int damage = 10;
+    [SerializeField] private int damageAmount = 10;
+    [SerializeField] private float knockbackForce = 5f;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider collider)
     {
-        if (other.CompareTag("Player"))
-        {
-            var health = other.GetComponent<PlayerCondition>();
-            if (health != null)
-                health.TakeDamage(damage);
+       
+        if (collider.CompareTag("Player"))
+        {   
+            //체력감소
+            var player = collider.GetComponent<PlayerCondition>();
+            if (player != null)
+            {
+                player.TakeDamage(damageAmount);
+            }
+           //넉백
+            Rigidbody playerRb = collider.GetComponent<Rigidbody>();
+            if (playerRb != null)
+            {
+                Vector3 knockDir = (collider.transform.position - transform.position).normalized;
+                knockDir.x = 4f;
+                knockDir.y = 1.5f;
+
+                playerRb.AddForce(knockDir * knockbackForce, ForceMode.Impulse);
+            }
+
+            Debug.Log($"{damageAmount} 받음!");
         }
     }
-
-
 }
